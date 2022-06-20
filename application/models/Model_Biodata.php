e6ab53a764cb27222c62bb77d55fcca47d1dbb52
LaurentiusDandiAndhika <dandiandhika014@gmail.com>
2022-06-20 14:29:21 +0700

    upload tugas

--- application/models/Model_Biodata.php
+++ application/models/Model_Biodata.php
@@ -2,8 +2,10 @@
 
 class Model_biodata extends CI_Model{
 
-    public function biodata(){
-        $hasil = "Laurentius Dandi Andhika";
-        return $hasil;
+    public function biodata()
+    {
+    $hasil ="Laurentius Dandi Andhika";
+    return $hasil;
     }
+
 }
--- application/views/view_biodata.php
+++ application/views/view_biodata.php
@@ -1,14 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Biodata</title>
+    <meta charset="utf-8">
+    <title>Welcome to CodeIgniter</title>
 </head>
 <body>
-    <h1><?= $title;?>Perkenalkan</h1>
+
+<div id="container">
+    <h1>Perkenalkan </h1>
     <p>Nama : Laurentius Dandi Andhika</p>
     <p>Hobi : Merawat Burung Berkicau</p>
+</div>
+
 </body>
-</html>
+</html> 
